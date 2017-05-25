83f9a3fc189fa7e8c22e1a72ad6786651af09f30
raafaadg <raafaadg@gmail.com>
2017-05-25 20:28:06 -0300

    att
    att

--- Exe5.m
+++ Exe5.m
@@ -1,33 +1,53 @@
-clear all;close all;clc;
+clear all;
+close all;clc;
 
 syms x1 x2 a
 format long
+
+scen=input('Informe 0(zero) para o Problema do Slide e 1(um) para o Exercício 5\n');
+switch scen
+    case 0
+        [xx,yy]=meshgrid(0:(5/3)/10:5,0:(3/5)/10:3);
+        x=[0 0];
+        zz=xx.^2+4.*yy.^2-8.*xx-16.*yy;
+        f(x1,x2)=x1^2+4*x2^2-8*x1-16*x2;
+        alfa=[0 .1 .12 .15];
+    case 1
+        [xx,yy]=meshgrid(-7:.25:7,-20:.5:36);
+        x=[-6 4];
+        f(x1,x2)=2*x1^3+(x2-8)^2+exp(-x1);
+        zz=2.*xx.^3+(yy-8).^2+exp(-xx);
+        alfa=[0 .01 .03 .05];
+end
 figure(1);
-[xx,yy]=meshgrid(0:.2:5);
-
-% x=[0 0];
-% zz=xx.^2+4.*yy.^2-8.*xx-16.*yy;
-% f(x1,x2)=x1^2+4*x2^2-8*x1-16*x2;
-
-x=[4 4];
-zz=2.*xx.^3+(yy-8).^2+exp(-xx);
-f(x1,x2)=2*x1^3+(x2-8)^2+exp(-x1);
-
-surfc(xx,yy,zz);
+meshc(xx,yy,zz)
 
 figure(2)
-contour(xx,yy,zz);
+contour(xx,yy,zz,'DisplayName','Curvas');
 hold on
 grid on
 disp('Processo de minimazação');
-metodo=0;
+metodo=input('Selecione o Método -> 0 = Gradiente; 1 = Newton\n');
 
 switch metodo
     case 0
-        [X,DF]=Gradiente(f,x,0)
+        disp('Método do Gradiente Selecionado');
+        uni=input('Selecione o Método de Busca Unidimensional -> 0 = Bisseção; 1 = Seção Aurea\n');
+        for it=1:size(alfa,2)
+            [X,DF]=Gradiente(f,x,alfa(it),uni);
+            plot(X(:,1),X(:,2),'LineWidth',1,...
+            'Marker','o',...
+            'MarkerSize',5,...
+            'DisplayName',['alfa = ' num2str(alfa(it))])           
+            hold on
+            
+        end
     case 1
+        disp('Método de Newton Selecionado');
         [X,DF]=newton(f,x)
+        plot(X(:,1),X(:,2),'LineWidth',1,...
+            'Marker','o',...
+            'MarkerSize',5)
+        hold on    
 end
-
-plot(X(:,1),X(:,2),'go')
-plot(X(:,1),X(:,2),'k','LineWidth',1)
+legend('show')
--- newton.m
+++ newton.m
@@ -11,11 +11,13 @@ h12(x1,x2)=diff(gf1(x1,x2),x2);
 h21(x1,x2)=diff(gf2(x1,x2),x1);
 h22(x1,x2)=diff(gf2(x1,x2),x2);
 
-H(x1,x2)=[h11(x1,x2) h12(x1,x2);h21(x1,x2) h22(x1,x2)];
+
 df(k,:)=[double(gf1(x(k,1),x(k,2))) double(gf2(x(k,1),x(k,2)))];
 tic;
-while((abs(double(df(k,1)))>1e-2)&&(abs(double(df(k,2)))>1e-2))
-x(k+1,:)=x(k,:)-((VPA(H^-1))*df(k,:)')';
+while((abs(double(df(k,1)))>1e-2)||(abs(double(df(k,2)))>1e-2))
+H(x1,x2)=[h11(x(k,1),x(k,2)) h12(x(k,1),x(k,2));...
+    h21(x(k,1),x(k,2)) h22(x(k,1),x(k,2))];   
+x(k+1,:)=x(k,:)-((double(H^-1))*df(k,:)')';
 k=k+1;
 df(k,:)=[double(gf1(x(k,1),x(k,2))) double(gf2(x(k,1),x(k,2)))];
 end
